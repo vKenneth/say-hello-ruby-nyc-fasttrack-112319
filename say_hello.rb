bff6a7dc362dffc8a33bb4369e1265481b34733a
K e n V <kenneth_valdivia@yahoo.com>
2019-11-26 00:46:16 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,4 @@
 # Build your say_hello method here
-def greeting(name="Gabriela", language="Ruby")
-  puts "Hello, #{name}. We heard you are a great #{language} programmer."
-end
+def say_hello(name="Gabriela")
+  puts 
+  
